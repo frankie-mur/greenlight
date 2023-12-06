257031d10cb70b87201d83df67f379f97b04147f
Frankie <frankidatank@gmail.com>
2023-12-05 19:24:09 -0700

    add CreateUser handler

---
+++ cmd/api/users.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/frankie-mur/greenlight/internal/data"
+	"github.com/frankie-mur/greenlight/internal/validator"
+)
+
+func (app *application) createUserHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	//read json from request
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	//Create our inital User
+	user := &data.User{
+		Name:      input.Name,
+		Email:     input.Email,
+		Activated: false,
+	}
+
+	err = user.Password.Set(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+	v := validator.New()
+	//Validate our user data
+	if data.ValidateUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Insert the user data into the database.
+	err = app.models.Users.Insert(user)
+	if err != nil {
+		switch {
+		// If we get a ErrDuplicateEmail error, use the v.AddError() method to manually
+		// add a message to the validator instance, and then call our
+		// failedValidationResponse() helper.
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// Write a JSON response containing the user data along with a 201 Created status
+	// code.
+	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
